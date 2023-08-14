d5af5d642fc3c8aaaf454f922021e95cd8fec162
rafa <arnoldSaavedra.93@gmail.com>
2023-08-14 12:25:04 -0500

    Mejora

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,8 +1,4 @@
 export { default as getPeopleSwapi } from "./swapi/swapi-people";
 export { default as getPlanetSwapi } from "./swapi/swapi-planet";
-export { default as addPerson } from "./add-person";
-export { default as getPeople } from "./get-people";
-export { default as getPerson } from "./get-person";
 export { default as addHeroe } from "./heroes/heroe-add";
 export { default as allHeroe } from "./heroes/hores-all";
-export { default as updateHero } from "./heroes/heroe-update";
